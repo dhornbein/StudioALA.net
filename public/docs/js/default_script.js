d4486925b8a00af557b8fe363f022a066cc2d572
Drew Hornbein <dhornbein@gmail.com>
2012-05-26 11:31:15 -0400

    updates and lower case image names

---
+++ public/docs/js/default_script.js
@@ -0,0 +1,28 @@
+$(window).load(function(){
+
+	$('.fitBaseline').each(fitBaselineHieght);
+
+	//test for cycle
+	if(jQuery.isFunction(jQuery.fn.cycle)){
+		$('#slideshow').cycle({ 
+			fit: true,
+			height: 400,
+			fx:     'fade', 
+			speed:  'fast', 
+			timeout: 0, 
+			next:   '.navRight', 
+			prev:   '.navLeft',
+			pager: '.navPager'
+		}).show();
+	}
+});
+
+function fitBaselineHieght(){
+	var targetCss = 'margin';
+	var height = $(this).height();
+	var baseline = 18;
+	var desiredHeight = (Math.ceil(height / baseline)) * baseline;
+	var padding = desiredHeight - height;
+
+	$(this).css(targetCss + 'Bottom',padding);
+}
