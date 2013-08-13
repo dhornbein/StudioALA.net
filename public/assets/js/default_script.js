7c2d67a4c213bbc06c3b04c1812016bda620da4b
Drew Hornbein <dhornbein@gmail.com>
2013-08-13 15:43:19 -0400

    apply new visual style

--- public/assets/js/default_script.js
+++ public/assets/js/default_script.js
@@ -1,21 +1,64 @@
 $(document).ready(function() {
-   
-	$('.fitBaseline').each(fitBaselineHieght);
 
 	$('.fit').fitText();
+	$('a[href^=#]').smoothmove();
 
-	//test for cycle
-	if(jQuery.isFunction(jQuery.fn.cycle) && $('#slideshow') ){
-		$('#slideshow').cycle({ 
-			fit: true,
-			height: 400,
-			fx:     'fade', 
-			speed:  'fast', 
-			timeout: 0, 
-			next:   '.navRight', 
-			prev:   '.navLeft',
-			pager: '.navPager'
-		}).show();
-	}
+	$(document).foundation();
+
+    $('.img-gallery').magnificPopup({
+      delegate: 'a', // child items selector, by clicking on it popup will open
+      type: 'image',
+      gallery:{enabled:true}
+	});
 
 });
+
+/*
+
+	SmoothMove
+	A simple page scrolling jQuery Plugin	
+	@author Collin Olan
+
+*/
+(function( $ ){
+
+	$.fn.smoothmove = function( options ) {
+
+		var settings = {
+			'speed': 300,
+			'offset': 0,
+			'target': null
+		};
+
+		if (this.data("target") !== undefined && settings.target !== null ) {
+			settings.target = this.data("target");
+		}
+
+		return this.each(function() {
+			if ( options ) { 
+				$.extend( settings, options );
+			}
+
+			$(this).click(function(e) {
+                var scrollTarget;
+				if (settings.target !== null) {
+					scrollTarget = settings.target;
+				} else {
+					scrollTarget = $(this).attr("href");
+				}
+
+				if ($(scrollTarget).length > 0) {
+					var destination = $(scrollTarget).offset().top;
+					$("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination - settings.offset}, settings.speed );
+					e.preventDefault();
+				} else {
+					return;
+				}
+
+			});
+
+		});
+
+	};
+
+})( jQuery );
